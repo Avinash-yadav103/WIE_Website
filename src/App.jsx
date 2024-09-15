d7c96fc50168e332f7169e4016e5314868b5b588
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-09-15 16:03:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      This is my App
+      Hello there how are you 
+    </>
+  )
+}
+
+export default App
